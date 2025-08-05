d2c51dff8b749306e96409393047400dfb275430
Helder Lagrisola <helderlagrisola'gmail.com>
2025-08-05 20:24:54 +0100

    Style up hero section

---
+++ main.js
@@ -0,0 +1,22 @@
+let intro  = document.querySelector(".intro");
+let buttonPerson = document.getElementById("person")
+
+
+
+buttonPerson.onclick = function idk() {
+    intro.innerHTML =  `
+   <h1>Helder Lagrisola</h1>
+    <h3>Front End Web Developer</h3>
+  <p>I’m a passionate Front-End Web Developer skilled in HTML, CSS, JavaScript, and modern frameworks like React. I create responsive, user-friendly websites with a focus on great design and smooth functionality. I enjoy turning ideas into interactive digital experiences and am always eager to learn new technologies.</p>
+ 
+    <div class="social">
+        <i class="bi bi-facebook"></i>
+        <i class="bi bi-github"></i>
+        <i class="bi bi-instagram"></i>
+        <i class="bi bi-linkedin"></i>
+        <i class="bi bi-download"></i>
+      
+    </div>
+    
+`
+}
